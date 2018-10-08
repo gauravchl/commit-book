7eaca59a0d070eaf5c815858c888305f9f69929c
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2018-10-08 12:43:48 +0530

    Initial commit

---
+++ spec/commit-book-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CommitBookView from '../lib/commit-book-view';
+
+describe('CommitBookView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
